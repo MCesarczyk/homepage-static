4a6fa861fbd98b54b48ac955888c95efcd845920
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2022-02-15 02:50:22 +0100

    next adjustments, add link to title and test to it

--- src/Header/index.js
+++ src/Header/index.js
@@ -3,13 +3,18 @@ import './style.css';
 
 const Header = () => (
   <header className='header'>
-      <img
-        className='header__titleImage'
-        src={portrait} alt="portrait"
-      />
+    <img
+      className='header__titleImage'
+      src={portrait} alt="portrait"
+    />
+    <a
+      data-testid="header-link"
+      href='#contact'
+    >
       <h1 className='header__title'>
         Cesarczyk.dev
       </h1>
+    </a>
   </header>
 
 );
