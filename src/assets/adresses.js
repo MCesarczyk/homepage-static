01a4ab061c4521b4b43277676e5d4d36639f5099
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2022-03-25 01:48:55 +0100

    replace codepen footer icon with upwork one, update snapshot

--- src/assets/adresses.js
+++ src/assets/adresses.js
@@ -1,6 +1,7 @@
 export const MAIL_ADRESS = "michal.cesarczyk@gmail.com";
 export const GITHUB_ADRESS = "https://github.com/MCesarczyk";
 export const LINKEDIN_ADRESS = "https://www.linkedin.com/in/micha%C5%82-cesarczyk-0820589a";
+export const UPWORK_ADRESS = "https://www.upwork.com/freelancers/~01ec95abd3619f24c6";
 export const CODEPEN_ADRESS = "https://codepen.io/bikeoholic";
 export const FACEBOOK_ADRESS = "https://www.facebook.com/mich.cesarz/";
 export const INSTAGRAM_ADRESS = "https://www.instagram.com/mich_cesarz/";
